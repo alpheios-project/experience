a821360bc014bf3b184f4bb06ee4a6aceb2e2001
Kirlat <kirlat@yula.media>
2017-12-27 16:33:46 +0400

    Added ObjectMonitor into Experience to track request objects.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Experience from './experience'
 import Monitor from './monitor'
+import ObjectMonitor from './object-monitor'
 import Transporter from './transporter'
 import StorageAdapter from './local/storage'
 import TestAdapter from './remote/test-adapter'
@@ -7,6 +8,7 @@ import TestAdapter from './remote/test-adapter'
 export {
   Experience,
   Monitor,
+  ObjectMonitor,
   Transporter,
   StorageAdapter,
   TestAdapter
